fd3f5a077200a719af061557730047d60d6baba6
czmud <caleb.zmuda@protonmail.com>
2022-11-15 09:35:36 -0800

    11/15/22 practice

--- leet_code_practice/lc215_find_kth_largest.py
+++ leet_code_practice/lc215_find_kth_largest.py
@@ -1,5 +1,13 @@
 import heapq
+
+#read documentation for heapq and realized how .nlargest() works
+# heapify(nums) is no longer necessary, given .nlargest() is already doing a similar max heapify in the background
+# https://github.com/python/cpython/blob/3.11/Lib/heapq.py
 class Solution:
     def findKthLargest(self, nums, k):
-        heapq.heapify(nums)
         return heapq.nlargest(k, nums )[-1]
+
+nums1 = [1, 2, 3, 4, 5, 6, 7, 8]
+
+solution = Solution()
+print(solution.findKthLargest(nums1, 3))
