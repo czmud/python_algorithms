7ff2fc81938fee5b411dc814670e82f2c1b14f6d
czmud <caleb.zmuda@protonmail.com>
2022-11-16 09:27:46 -0800

    11/16/22 practice

--- leet_code_practice/lc215_find_kth_largest.py
+++ leet_code_practice/lc215_find_kth_largest.py
@@ -1,13 +1,41 @@
 import heapq
+import math
+import random
 
 #read documentation for heapq and realized how .nlargest() works
 # heapify(nums) is no longer necessary, given .nlargest() is already doing a similar max heapify in the background
 # https://github.com/python/cpython/blob/3.11/Lib/heapq.py
 class Solution:
-    def findKthLargest(self, nums, k):
+    def findKthLargest1(self, nums, k):
         return heapq.nlargest(k, nums )[-1]
+    def findKthLargest2(self, nums, k):
+        heapq.heapify(nums)
+        while len(nums) > k:
+            heapq.heappop(nums)
+        return nums[0]
+    def findKthLargest(self, nums, k):
+        p = random.choice(nums)
+        
+        g = []
+        e = []
+        l = []
+        for num in nums:
+            if num > p:
+                g.append(num)
+            elif num == p:
+                e.append(num)
+            else:
+                l.append(num)
+        
+        if len[g] >= k:
+            return self.findKthLargest( g, k )
+        elif len[l] > len[nums] - k:
+            return self.findKthLargest( l, k - len(e) - len(g) )
+        return e[0]
 
-nums1 = [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+nums1 = [11, 12, 13, 14, 15, 16, 17, 18]
 
 solution = Solution()
-print(solution.findKthLargest(nums1, 3))
+print(solution.findKthLargestQuickSelect(nums1, 3))
