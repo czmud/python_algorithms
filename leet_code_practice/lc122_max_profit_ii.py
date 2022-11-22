442af753b85b901c82c30aa7c03b97fd374ff7cc
czmud <caleb.zmuda@protonmail.com>
2022-11-22 09:26:26 -0600

    11/22/22 practice

---
+++ leet_code_practice/lc122_max_profit_ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxProfit1(self, prices):
+        profit = 0
+        for i in range(1,len(prices)):
+            profit += max(0, prices[i] - prices[i-1])
+        return profit
+
+    def __init__(self):
+        self.memo = {
+            0: 0,
+            1: 0
+        }
+    # Let's use DP to brute force the solution just for fun
+    def maxProfit(self, prices):
+        if len(prices) in self.memo:
+            return self.memo[len(prices)]
+        
+        buy = prices[0]
+        max_added = 0
+        for i in range(1, len(prices)):
+            # buy
+            max_added = max(max_added, prices[i] - buy + self.maxProfit(prices[i+1:]))
+            # don't buy
+            max_added = max(max_added, self.maxProfit(prices[i:]))
+
+        self.memo[len(prices)] = max_added
+        return self.memo[len(prices)]
+
+
+
+
